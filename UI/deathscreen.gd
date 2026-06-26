d9cbdf0ef2bf6c2fa01409c35bdc5a639d0e4c18
n2ize <niki.ans@gmx.de>
2026-06-26 14:02:31 +0200

    bug fixes of mainmenu

--- UI/deathscreen.gd
+++ UI/deathscreen.gd
@@ -14,3 +14,9 @@ func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	queue_free()
 	
+
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,11 +1,14 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
-	$VBoxContainer/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
-
-func _on_play_pressed():
+	if not $VBoxContainer/VBoxContainer/PlayButton.pressed.is_connected(_on_play_button_pressed):
+		$VBoxContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
+	print("main menu ready called")
+	
+func _on_play_button_pressed() -> void:
+	print("play pressed")
 	get_tree().change_scene_to_file("res://Rooms/level_generator.tscn")
 
-func _on_quit_pressed():
+
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
